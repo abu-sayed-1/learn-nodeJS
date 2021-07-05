c68717474a6bb91121b8eea389ee15f777865a6d
as ahmed <rakibasrr13@gmail.com>
2021-07-05 20:38:09 +0300

    Create and Export

---
+++ importAndExport/index.js
@@ -0,0 +1,23 @@
+const add = (a, b) => {
+    return a + b;
+};
+
+const sub = (a, b) => {
+    return a - b;
+};
+
+const multi = (a, b) => {
+    return a * b;
+};
+
+const division = (a, b) => {
+    return a / b;
+}
+
+const modular = (a, b) => {
+    return a % b;
+}
+
+const name = "as Rakib";
+
+module.exports = { name, add, sub, multi, division, modular };
---
+++ importAndExport/oper.js
@@ -0,0 +1,9 @@
+const { add, name, sub, multi, division, modular } = require("./index");
+
+console.log(add(60, 20));
+console.log(sub(10, 9));
+console.log(multi(20, 3));
+console.log(division(100, 11));
+console.log(modular(50, 4));
+console.log(name);
+
