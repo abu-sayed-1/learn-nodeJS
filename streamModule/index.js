504353ca5a610ab3b9e7a6c72ea3894b265411e4
as ahmed <rakibasrr13@gmail.com>
2021-07-15 20:43:46 +0300

    Write the stream method very briefly

--- streamModule/index.js
+++ streamModule/index.js
@@ -5,13 +5,13 @@ const server = http.createServer();
 
 server.on("request", (req, res) => {
 
-    //This is not a stream method
+    //This is not a stream method/ 01
     fs.readFile("input.txt", (err, data) => {
         if (err) return console.error(err);
         res.end(data.toString());
     });
 
-    //This is the stream method
+    //This is the stream method /02
     const stream = fs.createReadStream("input.txt");
     stream.on("data", (chunkData) => {
         res.write(chunkData);
@@ -23,6 +23,11 @@ server.on("request", (req, res) => {
         console.log(err)
         res.end("document is not found!")
     });
+
+    // Write the stream method very briefly /03
+    const stream = fs.createReadStream("input.txt");
+    stream.pipe(res);
+
 });
 
 server.listen(4000);
